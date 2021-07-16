3656a9f9b7c7bfb4114f25acaf2d4f8c6f47a657
Joel Wetzell <jwetzell@yahoo.com>
2021-07-16 14:10:09 +0000

    Add QMicCue class. Closes #10

---
+++ QControlKit/Cue/QMicCue.cs
@@ -0,0 +1,37 @@
+using QControlKit.Constants;
+
+namespace QControlKit.Cue
+{
+    public class QMicCue : QCue
+    {
+        public int channelOffset
+        {
+            get
+            {
+                return (int)propertyForKey(QOSCKey.ChannelOffset);
+            }
+        }
+
+        public int channels
+        {
+            get
+            {
+                return (int)propertyForKey(QOSCKey.Channels);
+            }
+	    set
+	    {
+                setProperty(value, QOSCKey.Channels);
+	    }
+        }
+        
+	public void setDefaultLevels()
+        {
+            workspace.sendMessage($"/cue_id/{this.propertyForKey(QOSCKey.UID)}/setDefaultLevels");
+        }
+
+        public void setSilientLevels()
+        {
+            workspace.sendMessage($"/cue_id/{this.propertyForKey(QOSCKey.UID)}/setSilentLevels");
+        }
+    }
+}
